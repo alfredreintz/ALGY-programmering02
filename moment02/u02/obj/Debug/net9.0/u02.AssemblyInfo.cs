010b18e4600b820bd9a3b9c3159deb3601dfddec
alfredreintz <alfred.reintz@icloud.com>
2025-01-31 12:29:37 +0100

    changed place

--- moment02/u02/obj/Debug/net9.0/u02.AssemblyInfo.cs
+++ moment02/u02/obj/Debug/net9.0/u02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("u02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0df8b87c1640e6c3c24433a800b7893d8a1659")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0986adb32e2d517522116a3c64fcac75b80513")]
 [assembly: System.Reflection.AssemblyProductAttribute("u02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("u02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

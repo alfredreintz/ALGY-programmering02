87516c892deff525a0e0942ef3e01a624057aa01
alfredreintz <alfred.reintz@icloud.com>
2025-02-28 11:38:38 +0100

    h

--- SpaceShooter/obj/Debug/net8.0/SpaceShooter.AssemblyInfo.cs
+++ SpaceShooter/obj/Debug/net8.0/SpaceShooter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpaceShooter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63b076e5f3bfb051f65062b1529b16de25144a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9285f9a25c8ad21f2c56fc63a623cd658f99bbd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpaceShooter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpaceShooter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
